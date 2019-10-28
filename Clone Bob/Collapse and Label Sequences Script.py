89460ffaa3cb24db9ee3b07cf88b88689222c93b
thsiao17 <56963524+thsiao17@users.noreply.github.com>
2019-10-28 12:45:44 +0200

    Update Collapse and Label Sequences Script.py

--- Clone Bob/Collapse and Label Sequences Script.py
+++ Clone Bob/Collapse and Label Sequences Script.py
@@ -16,55 +16,6 @@ import sshtunnel
 import pickle
 
 import json
-from pandas.io.json import json_normalize
-
-import plotly.express as px
-import json
-import pickle
-import pandas as pd
-
-
-seq_map = dict()
-sequnces_data_frame = pd.read_pickle('C:\\artathon\\clone_509919_sequences.pkl')
-sequnces_data_frame['tree_layer'] = None
-sequnces_data_frame['tree_order'] = None
-
-f = pd.read_pickle('C:\\artathon\\clone_509919_tree.pkl')
-#json_obj = json.loads(f.tree)
-json_obj = 0
-for jsn in f.tree:
-    json_obj = json.loads(jsn)
-#f= open("C:\\artathon\\clone_509919_tree.json","w+")
-#f.write(json_obj.__str__())
-#f.close()
-
-#print(f.tree)
-def start_traverse(d, path=None):
-    for item, node in json_obj.items():
-        if item == "tree":
-            traverse(node)
-                    
-def traverse(node, level = 0, order = 0):
-    seq_ids = {"0" : 0}
-    if len(node['data']['seq_ids']) > 0:
-        seq_ids = node['data']['seq_ids']
-
-    #print(seq_ids.keys())
-    for seq in seq_ids.keys():
-        sequnces_data_frame['tree_layer'].loc[sequnces_data_frame['seq_id'] == seq] = level
-        sequnces_data_frame['tree_order'].loc[sequnces_data_frame['seq_id'] == seq] = order
-
- #   print("CHILDREN --> " + len(node['children']).__str__())
-    order = 0
-    for child in node['children']:
-        traverse(child, level+1, order)
-        order += 1
-
-start_traverse(json_obj)
-
-sequnces_data_frame.to_pickle("C:\\artathon\\clone_509919_sequences_result.pkl")
-
-
 ###download the clones table from mysql database
 
 # ssh variables
@@ -108,9 +59,11 @@ clone_509919 = D207_ALL_with_tissue.loc[D207_ALL_with_tissue['clone_id'] == 5099
 ###collapse sequences using pandas merge inner
 clone_509919_collapsed = clone_509919.merge(D207_collapsed_table, how='inner', left_on='ai', right_on='seq_ai')
 
+###save collapsed sequences dataframe
+clone_509919_collapsed.to_pickle("path to save result clone_509919_collapsed.pkl")
 
-
-
+###save clones table dataframe
+clones_table.to_pickle("path to save result clones_table.pkl")
 
 ###iterate through the json string for clone 509919 and label the sequences with the tree layer they're found in
 seq_map = dict()
